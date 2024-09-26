24752e0d687399ad84687822efeb9ece34caea23
David Almeida <dgfa1996@outlook.pt>
2024-09-26 15:18:39 +0100

    update

--- FolderTest3/teste_01.js
+++
@@ -1,7 +0,0 @@
-const base64String = "ZnVuY3Rpb24gbW9zdHJhck1lbnNhZ2VtKCkgew0KICAgIGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdtZW5zYWdlJykuaW5uZXJUZXh0ID0gIk9sYSwgZXN0YSB1bWEgbWVuc2FnZW0gZXhoaWJpZGEgdmlhIEphdmFTY3JpcHQhIjsNCn0=";
-
-// Decodificar Base64
-const decodedString = atob(base64String);
-
-// Executar o código decodificado
-eval(decodedString);
