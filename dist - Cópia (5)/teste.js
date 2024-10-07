820ccab3587f01a49da00476cd86d43ea07a1f7d
David Almeida <dgfa1996@outlook.pt>
2024-10-07 10:37:45 +0100

    update 2.1.6

---
+++ dist - Cópia (5)/teste.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Test = () => {
+  return (
+    <div>
+      <h1>This is a Test Component</h1>
+      <p>This component is a simple example.</p>
+    </div>
+  );
+};
